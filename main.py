7b7b8b208e5b0ab50855cd6b57e053a803807e52
Androuny <cetri.brali@gmail.com>
2024-03-19 20:32:18 +0200

    server now tracks last packet timestamp, not client

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, send_from_directory
 from flask_socketio import SocketIO, emit
 from flask_minify import Minify
+import time
 
 app = Flask(__name__)
 Minify(app=app, html=True, js=True, cssless=True)
 app.config['SECRET_KEY'] = '2e9ue8u39hugt4hu9grn9uf2n9u31eniu31duinfenuigr2475'
 socketio = SocketIO(app)
 
+last_packet_timestamp = 0
 
 @app.route("/")
 def index():
@@ -20,6 +22,9 @@ def api_schema():
 @app.route("/api/submit", methods=["POST"])
 def api_submit():
     if request.method == "POST":
+        global last_packet_timestamp
+
+        last_packet_timestamp = time.time() * 1000 # because js timestamps are in miliseconds not in seconds lol
         print(request.data)
         temp = request.json["temperature"]
         hum = request.json["humidity"]
@@ -32,7 +37,7 @@ def api_submit():
 @socketio.on('connect')
 def test_connect(auth):
     print(f'Websocket client connected: {request.remote_addr}')
-    emit('conn_response', {'data': 'Connected'})
+    emit('conn_response', {'data': 'Connected',"last_packet_timestamp":last_packet_timestamp})
 
 @socketio.on('disconnect')
 def test_disconnect():
